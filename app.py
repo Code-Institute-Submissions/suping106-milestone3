383472dc2560a20917a8c8641c69f18ba5093a84
suping106 <lou_suping@yahoo.com>
2021-05-13 01:42:29 +0000

    Allow user to add new recipe

--- app.py
+++ app.py
@@ -99,9 +99,25 @@ def logout():
     return redirect(url_for("login"))
 
 
-@app.route("/add_recipe")
-def add_recipe():
-    return render_template("add_recipe.html")
+@app.route("/add_recipe", methods=["GET", "POST"])
+def add_recipe():    
+    if request.method == "POST":        
+        recipe = {
+            "category_name": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "recipe_image": request.form.get("recipe_image"),
+            "recipe_ingredients": request.form.get("recipe_ingredients"),
+            "recipe_method": request.form.get("recipe_method"),
+            "recipe_serves": request.form.get("recipe_serves"),
+            "recipe_time": request.form.get("recipe_time"),
+            "created_by": session["user"]
+        }
+        mongo.db.recipes.insert_one(recipe)
+        flash("Recipe Successfully Added")
+        return redirect(url_for("get_recipes"))
+
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_recipe.html", categories=categories)
 
 
 if __name__ == "__main__":
